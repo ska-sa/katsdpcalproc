9edcf2d76ce893243ca20f91d93e5fd4bc555210
Ludwig Schwardt <ludwig@ska.ac.za>
2022-01-19 16:53:14 +0200

    Allow non-stationary flux, noise var and gains
    This makes the data more realistic. The source flux varies as a function
    of frequency, as does the SEFD. In addition, multiply the resultant
    sum with a frequency-dependent gain. Some channels are zeroed to
    indicate flagged data (use an actual RFI mask from 1556574656).
    Rework the CRB to cater for flagged and non-stationary data. It seems
    to work well :-) Add an experiment to test this.

--- tests/benchmark_delay.py
+++ tests/benchmark_delay.py
@@ -33,11 +33,22 @@ def experiment(flux=10., SEFD=400., dump_period=DUMP_PERIOD, channels=CHANNELS,
     freq = 0.99 * freq
     phase = 2. * np.pi * np.random.rand(K)
     noise = np.random.randn(K, N) + 1j * np.random.randn(K, N)
-    crlb = 6.0 / (SNR * N * (N * N - 1.0))
+    if window is None:
+        SNR_sum = SNR * N
+        N_range = N
+    else:
+        gate = np.nonzero(window)[0]
+        SNR_eff = np.zeros_like(SNR)
+        SNR_eff[gate] = SNR[gate]
+        SNR_sum = np.sum(SNR_eff)
+        N_range = gate[-1] - gate[0] + 1
+    crlb = 6.0 / (SNR_sum * (N_range * N_range - 1.0))
 
     n = np.arange(N, dtype=float)
     angle = np.outer(n, freq) + phase
     x = ampl * np.exp(1j * angle.T) + np.sqrt(noise_var / 2) * noise
+    if window is not None:
+        x *= np.atleast_2d(window)
 
     # Circular mean of phase difference
     cmpd = mean_phase_diff(x)
@@ -93,6 +104,42 @@ ax.legend(lines + crline,
 ax.set_xlabel('Number of samples (N)')
 ax.set_ylabel('Frequency standard deviation relative to 1/N')
 ax.set_title('Frequency (delay) estimator performance')
-fig.savefig('delay_estm_vs_N.pdf')
+fig.savefig('delay_estm_vs_N.png')
+
+t = np.arange(CHANNELS) / CHANNELS
+flux = 16 * np.exp(-0.65 * np.log(t + 1))
+sefd = 500 * np.exp(-0.3 * np.log(t + 1))
+gain = np.full_like(t, 0.01)
+for harmonic in range(1, 11, 2):
+    gain += np.sin(2 * np.pi * t * harmonic / 2) / harmonic
+gain /= np.median(gain)
+gate = np.zeros_like(t)
+gate_transitions = [0, 50, 81, 86, 93, 135, 190, 203, 279, 282, 338,
+                    536, 657, 681, 793, 901, 910, 926, 955, 968, 972, 1024]
+gate_scale = int(CHANNELS / gate_transitions[-1])
+segm_start = gate_transitions[:-1]
+segm_end = gate_transitions[1:]
+for n, (b, e) in enumerate(zip(segm_start, segm_end)):
+    gate[gate_scale * b : gate_scale * e] = float(n % 2 == 1)
+gain *= gate
+
+fluxes = np.array([0.1, 0.2, 0.5, 1., 2., 5., 10., 20., 50., 100.])
+scaled_std = np.empty((len(fluxes), 6))
+for n, flux in enumerate(fluxes):
+    scaled_std[n] = experiment(flux=flux, SEFD=sefd, window=gain)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+log_fluxes = np.log10(fluxes)
+crline = ax.semilogy(log_fluxes, scaled_std[:, 0], 'k--', marker='o')
+lines = ax.semilogy(log_fluxes, scaled_std[:, 1:], marker='.')
+ax.xaxis.set_ticks(log_fluxes)
+ax.xaxis.set_ticklabels(['{:g}'.format(fl) for fl in fluxes])
+ax.set_xlim(log_fluxes[0], log_fluxes[-1])
+ax.legend(lines + crline,
+          ('Ludwig', 'Laura', 'Lindsay', 'SKA', 'Secant', 'Best (CRLB)'))
+ax.set_xlabel('Calibrator flux (Jy)')
+ax.set_ylabel('Frequency standard deviation relative to 1/N')
+ax.set_title('Frequency (delay) estimator performance')
+fig.savefig('delay_estm_realistic.png')
 
 plt.show()
